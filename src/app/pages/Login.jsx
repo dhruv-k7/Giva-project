431215f608f8f090f5dfad6d545505358780a1f1
Dhruv Khandelwal <105049552+dhruv-k7@users.noreply.github.com>
2022-09-08 12:37:06 +0530

    Update Login.jsx

--- src/app/pages/Login.jsx
+++ src/app/pages/Login.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import LoginDetails from "./components/LoginDetails.jsx";
+import { useNavigate } from "react-router-dom";
 
 function Login(props){
+    let navigate = useNavigate();
+    function handleClick(){
+        navigate("/");
+    }
 
     return(    
         <div>
@@ -10,6 +15,7 @@ function Login(props){
                 <h1>Login</h1>
             </header>
             <LoginDetails findUser={props.findUser}/>
+            <p>Do not have an account? <button onClick={handleClick}>Sign Up</button></p>
         </div>
     ) 
 }
