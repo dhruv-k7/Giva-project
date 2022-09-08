8d69dea3bff4699891db12103a900d31daeef01a
Dhruv Khandelwal <105049552+dhruv-k7@users.noreply.github.com>
2022-09-08 12:36:14 +0530

    Update Signup.jsx

--- src/app/pages/Signup.jsx
+++ src/app/pages/Signup.jsx
@@ -1,15 +1,21 @@
 import React,{useState} from "react";
 import ReactDOM from "react-dom";
 import InputDetails from "./components/InputDetails.jsx";
+import { useNavigate } from "react-router-dom";
 
 function Signup(props){
-    
+    let navigate = useNavigate();
+    function handleClick(){
+        navigate("/login");
+    }
+
     return(    
         <div>
             <header>
                 <h1>Sign up</h1>
             </header>
             <InputDetails addUser={props.addUser}/>
+            <p>Already have an account? <button onClick={handleClick}>Login</button></p>
         </div>
     ) 
 }
